3e6068b5ca2393fd8cc0c556f5c429f3cb471c99
Adam Kossakowski <akossakowski@unity-t.pl>
2024-06-24 15:27:49 +0200

    addtion of task component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,13 +1,38 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
 
   @Input({required: true}) name!: string;
+
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: "Master Angular",
+      summary: "Learn all the basics",
+      dueDate: '2025-12-31'
+    }, 
+    {
+      id: 't2',
+      userId: 'u2',
+      title: "Master Vue",
+      summary: "Learn all the basics",
+      dueDate: '2025-12-31'
+    }, 
+    {
+      id: 't3',
+      userId: 'u3',
+      title: "Master React",
+      summary: "Learn all the basics",
+      dueDate: '2025-12-31'
+    },
+  ]
 }
